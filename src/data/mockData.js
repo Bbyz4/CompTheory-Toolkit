8653fba816e6e617546482f585c222efa114979e
Gabriela6666 <pietrasgabriela6@gmail.com>
2026-05-12 01:25:31 +0200

    ui proposal

---
+++ src/data/mockData.js
@@ -0,0 +1,77 @@
+export const mockSubmissions = [
+  {
+    id: 1,
+    name: "Jon Snow",
+    taskName: "Task 1",
+    submissionTime: "2024-06-01 10:00",
+    status: "OK"
+  },
+  {
+    id: 2,
+    name: "Cersei Lannister",
+    taskName: "Task 2",
+    submissionTime: "2024-06-02 14:30",
+    status: "Pending"
+  },
+  {
+    id: 3,
+    name: "Jon Snow",
+    taskName: "Task 1",
+    submissionTime: "2024-06-03 09:15",
+    status: "Error"
+  },
+  {
+    id: 4,
+    name: "Anya Stark",
+    taskName: "Task 4",
+    submissionTime: "2024-06-04 11:45",
+    status: "OK"
+  },
+  {
+    id: 5,
+    name: "Daenerys Targaryen",
+    taskName: "Task 5",
+    submissionTime: "2024-06-05 16:20",
+    status: "Error"
+  },  
+];
+
+
+export const mockUsers = [
+  {
+    id: 1,
+    name: "Jon Snow",
+    email: ""
+  },
+  {
+    id: 2,
+    name: "Cersei Lannister",
+    email: ""
+  },
+  {
+    id: 3,
+    name: "Jaime Lannister",
+    email: ""
+  },
+];
+
+export const mockTasks = [
+  {
+    id: 1,
+    taskTitle: "Task 1",
+    taskDescription: "Create a CFG for palindromes over the alphabet {0,1}.",
+  },
+  {
+    id: 2,
+    taskTitle: "Task 2",
+    taskDescription: "Create a NFA for all strings over the alphabet {0,1}.",
+  }
+];
+
+export const mockAdmins = [
+  {
+    id: 1,
+    name: "Admin One",
+    email: "one@example.com"
+  }
+];
---
+++ src/scenes/submissions/Submissions.jsx
@@ -0,0 +1,14 @@
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import GradingIcon from '@mui/icons-material/Grading';
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+
+const Submissions = () => {
+  return (
+    <div className="submissions">
+      <h1>Submissions</h1>
+    </div>
+  );
+};
+
+export default Submissions;
